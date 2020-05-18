02bfa9b34fffd6ada274b333bef5261bc064f5ac
admin <admin@gmail.com>
2020-05-18 22:00:16 +0530

    ec2keychages

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,17 @@
 # Provider
-  provider "aws" {
-      access_key="AKIAJT4IKFNDUXVFXGHQ"
-      secret_key="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"
+ provider "aws" {
+      access_key="AKIAQORZLAFTV3Y4P4EP"
+      secret_key="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"
       region="ap-south-1"
+      skip_credentials_validation = true
+      skip_requesting_account_id = true
+      skip_metadata_api_check = true
 }
 
-resource "aws_instance" "good-morning"{
+resource "aws_instance" "example"{
   ami = "ami-0470e33cd681b2476"
   instance_type="t2.micro"
-  availability_zone = "ap-south-1a"
-  key_name="logiec2autoscaling"
+  count=1
   user_data = <<-EOF
    #!/bin/bash
    echo "Hello, World" > index.html
@@ -19,34 +21,15 @@ resource "aws_instance" "good-morning"{
   tags = {
      Name= "tf-example"
 }
-   ebs_block_device {
-    device_name = "/dev/xvdb"
-    volume_type = "gp2"
-    volume_size = 10
-  }
-
 }
 
-resource "aws_security_group_rule" "example"{
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id="sg-05596f05dc37cfa4c"
- }
-
-resource "aws_ebs_volume" "data-vol" {
- availability_zone = "ap-south-1a"
- size = 20
- tags = {
-        Name = "data-volume"
- }
-
+resource "aws_security_group" "instance"{
+  name="tf-example-instance"
+  ingress {
+    from_port=8080
+    to_port=8080
+    protocol="tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 }
-#
-resource "aws_volume_attachment" "good-morning-vol" {
- device_name = "/dev/sdc"
- volume_id = "${aws_ebs_volume.data-vol.id}"
- instance_id = "${aws_instance.good-morning.id}"
 }
+
